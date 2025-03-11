94714b0aa7c3e41174b2e1ff077fea8d78ce1706
SarathS95 <sarath9504@gmail.com>
2025-03-12 00:07:59 +1300

    responsive search

--- product.js
+++ product.js
@@ -1,12 +1,35 @@
+const searchInput = document.querySelector("#searchBox");
+let products = [];
+
 fetch('http://localhost:3000/products')
 .then((response) => response.json())
-.then((json) =>{
+.then((json) => {
+    products = json;
      json.forEach((product) => addProduct(product.title, product.description, product.price, product.category, product.type, product.image));
 })
+function productDisplay(filter) {
+    const items = document.getElementById("product");
+    const productCards = items.querySelectorAll(".product-card");
+    productCards.forEach(cards => {
+        let title =cards.querySelector(".card-title").innerText;
+
+        if(title.includes(filter)) {
+            cards.style.display = "block";
+        } else {
+            cards.style.display = "none"
+    }
+});
+}
+searchInput.addEventListener("input", (event) => {
+    productDisplay(event.target.value);
+});
+
 function addProduct(title, description,price, category, type, image) {
     // cloning the template
     const template = document.getElementById("product-card")
     .content.cloneNode(true);
+    
+    
     
     // Populating the template
     template.querySelector(".product-image").src = image;
@@ -19,4 +42,7 @@ function addProduct(title, description,price, category, type, image) {
 
     // Append card
     document.getElementById("product").appendChild(template);
+
+    
+    
 }
